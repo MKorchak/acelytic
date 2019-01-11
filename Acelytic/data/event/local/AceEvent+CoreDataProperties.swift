27112d70a629a575318544e28252d9fffaf41601
AlexeyPanyok <alexeypanyok@AlexeyPanyok.local>
2019-01-11 15:40:41 +0200

    changes

---
+++ Acelytic/data/event/local/AceEvent+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  AceEvent+CoreDataProperties.swift
+//  Acelytic
+//
+//  Created by AlexeyPanyok on 1/11/19.
+//  Copyright © 2019 ACE. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension AceEvent {
+    
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<AceEvent> {
+        return NSFetchRequest<AceEvent>(entityName: "Event");
+    }
+    
+    @NSManaged public var id: String?
+    @NSManaged public var name: String?
+    @NSManaged public var properties: String?
+    @NSManaged public var time: Double = Double(0)
+
+}
---
+++ Acelytic/data/event/local/AceEvent.swift
@@ -0,0 +1,14 @@
+//
+//  AceEvent.swift
+//  Acelytic
+//
+//  Created by AlexeyPanyok on 1/11/19.
+//  Copyright © 2019 ACE. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class AceEvent: NSManagedObject {
+    
+}
