58cb076b0b0c3f7108c81816ee07cbd8b7b18d33
AlexeyPanyok <alexeypanyok@AlexeyPanyok.local>
2019-01-14 17:30:31 +0200

    -changes

--- Acelytic/db/CoreDataStore.swift
+++ Acelytic/db/CoreDataStore.swift
@@ -6,9 +6,9 @@ class CoreDataStore {
     static let shared = CoreDataStore()
 
     lazy var persistentContainer: NSPersistentContainer = {
-        let modelURL = Bundle(for: type(of: self)).url(forResource: "Acelytic", withExtension: "momd")!
-//        let managedObjectModel = NSManagedObjectModel.mergedModel(from: nil)!
-        let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)!
+//        let modelURL = Bundle(for: type(of: self)).url(forResource: "Acelytic", withExtension: "momd")!
+        let managedObjectModel = NSManagedObjectModel.mergedModel(from: nil)!
+//        let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL)!
         let container = NSPersistentContainer(name: "Acelytic", managedObjectModel: managedObjectModel)
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in })
         return container
