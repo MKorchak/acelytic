2a5b3e34fce1fb1189dfc4b5762e567845766921
AlexeyPanyok <alexeypanyok@AlexeyPanyok.local>
2019-01-14 14:04:26 +0200

    Edit constants

--- Acelytic/C.swift
+++ Acelytic/C.swift
@@ -1,17 +1,17 @@
 import Foundation
 
-struct C {
-    static let START_SESSION = "ace_start_session"
-    static let END_SESSION = "ace_start_session"
+public struct C {
+    public static let START_SESSION = "ace_start_session"
+    public static let END_SESSION = "ace_start_session"
 
-    static let ACE_REVENUE_EVENT = "ace_revenue_event"
-    static let ACE_USER_PROPERTIES_EVENT = "ace_user_properties_event"
+    public static let ACE_REVENUE_EVENT = "ace_revenue_event"
+    public static let ACE_USER_PROPERTIES_EVENT = "ace_user_properties_event"
 
-    static let ACE_PRODUCT_ID = "ace_product_id"
-    static let ACE_REVENUE_TYPE = "ace_revenue_type"
-    static let ACE_PRICE = "ace_price"
-    static let ACE_CURRENCY = "ace_currency"
+    public static let ACE_PRODUCT_ID = "ace_product_id"
+    public static let ACE_REVENUE_TYPE = "ace_revenue_type"
+    public static let ACE_PRICE = "ace_price"
+    public static let ACE_CURRENCY = "ace_currency"
 
-    static let ACE_USER_ID_DEFAULTS = "ace_user_id_defaults"
-    static let ACE_USER_ID = "userId"
+    public static let ACE_USER_ID_DEFAULTS = "ace_user_id_defaults"
+    public static let ACE_USER_ID = "userId"
 }
